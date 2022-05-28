567d38d744e5ab9fe890bad1c86bd8fbb09ad311
An-JiYun <agy0512@gmail.com>
2022-05-28 17:16:39 +0900

    Nav Bar 완성(일까과연ㅎ)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <div>
-      <h1>Welcome!</h1>
+    <div className="App">
+      <nav>
+        <h1 className="logo">🟡로고</h1>
+        <ul className="click">
+          <li><a href="#!">여행지</a></li>
+          <li><a href="#!">여행계획</a></li>
+          <li><a href="#!">여행지추천</a></li>
+          <li><a href="#!">마이페이지</a></li>
+        </ul>
+        <div>
+          <ul className="login">
+            <div className="navSearch">
+              <input className="inputSearch" type="text"></input>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+              </svg>
+            </div>  
+            <li><a href="#!">LOGIN</a></li>
+            <li><a href="#!">JOIN US</a></li>
+          </ul>
+        </div>
+        
+      </nav>
     </div>
   );
 }
