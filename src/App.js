a15cbdb6423542c0b4a2ff6f0ec184f453f3c662
soyeeee2 <soyeeee2@gmail.com>
2022-07-05 21:14:09 +0900

    ddd

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import TDcontent from './component/TDcontent';
 import Navbar from './component/Navbar';
 import SelectDropBox from './component/SelectDropBox';
-
+import SortDropBox from './component/SortDropBox';
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           <h1>여행지</h1>
           <SelectDropBox />
           
-          <div className='dropdown'>dropdown box 자리</div>
+          <SortDropBox />
           <TDcontent />
           <TDcontent />
           <TDcontent />
--- src/component/TDcontent.js
+++ src/component/TDcontent.js
@@ -3,7 +3,8 @@ import styles from "./TDcontent.module.css"
 const TDcontent = () => {
   return(
       <div className={styles.TDcontentBox}>
-        <div className="hihi"></div>
+        <div className={styles.imgBox}></div>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid tenetur rerum cupiditate dolorem deserunt saepe exercitationem magni, quidem unde laborum architecto optio illum ex, ullam, perferendis nisi nobis a. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque illo aliquam, aperiam eius illum maxime modi exercitationem ex ipsa nisi culpa quia quisquam minima voluptatibus necessitatibus itaque vero. Enim, aperiam.</p>
       </div>
   )
 }
