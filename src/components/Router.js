8413df2ce735a3bb3d072185207463947fd5f3dd
eunbin777 <103042314+eunbin777@users.noreply.github.com>
2022-07-20 16:18:27 +0900

    router setup

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,13 +2,13 @@ import React, {useState} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TDhome from "../routes/TDhome";
 import TDdetail from "../routes/TDdetail";
+import MainPage from "../routes/MainPage";
 
-const AppRouter = () => {
+function AppRouter() {
   return (
     <Router>
       <Routes>
-        <TDhome></TDhome>
-        
+        <Route path="/" element={<MainPage />}></Route>
         <Route path="/TDhome" exact={true} element={<TDhome />}></Route>
         <Route path="/TDdetail" element={<TDdetail />}></Route>
       </Routes>
