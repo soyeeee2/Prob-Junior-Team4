c3009e82fb89d54fe4dffedc3b8168e13b9c441c
wjdtjdgns <105824163+wjdtjdgns@users.noreply.github.com>
2022-07-20 16:35:51 +0900

    add MapPage (#3)

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TDhome from "../routes/TDhome";
 import TDdetail from "../routes/TDdetail";
 import MainPage from "../routes/MainPage";
+import MapPage from "../routes/MapPage";
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
+        <Route path="/MapPage" element={<MapPage />}></Route>
         <Route path="/TDhome" exact={true} element={<TDhome />}></Route>
         <Route path="/TDdetail" element={<TDdetail />}></Route>
       </Routes>
