fb7e964e548061b3d2f8a433f44b791ab8e1fa31
soyeeee2 <92347149+soyeeee2@users.noreply.github.com>
2022-07-21 16:51:44 +0900

    Soyeeee2 (#4)
    * T-T
    
    * T-T
    
    * mainpage add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import styles from "../styles/Navbar.module.css"
 import {Link} from "react-router-dom";
 import TDhome from "../routes/TDhome";
+import Logo from '../assets/Logo.png';
 
 const Navbar = () => {
   return(
     <nav className={styles.navbar}>
       <div className={`${styles.navCon} ${styles.conLarge}`}>
-          <h1>🟡로고</h1>
+          <div className={styles.LogoWrap}>
+            <img src={Logo} width='45' height='27' />
+            <div className={styles.travel}>Travel</div>
+          </div>
           <ul className={styles.click}>
             <li><Link to="/TDhome" element={<TDhome />}>여행지</Link></li>
             <li><a href="#!">여행계획</a></li>
@@ -14,7 +18,7 @@ const Navbar = () => {
             <li><a href="#!">마이페이지</a></li>
           </ul>
           <div className={styles.navSearch}>
-            <input className={styles.inputSearch} type="text"></input>
+            <input className={styles.inputSearch} type="text" placeholder="지도로 검색하기"></input>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
             </svg>
--- src/components/TDheader.js
+++ src/components/TDheader.js
@@ -1,10 +1,17 @@
 import styles from '../styles/TDheader.module.css';
-
+import Diamond from '../assets/diamond-symbol.png';
 
 const TDheader = () => {
   return(
     <div className={styles.TDheader}>
       <h1>여행지</h1>
+      <div className={styles.DiamondWrap}>
+        <img className={styles.DiamondMedium} src={ Diamond } />
+        {/* <img className={styles.DiamondSmall} src={ Diamond }  /> */}
+        <img className={styles.DiamondLarge} src={ Diamond }  />
+        {/* <img className={styles.DiamondSmall} src={ Diamond }  /> */}
+        <img className={styles.DiamondMedium} src={ Diamond }  />
+      </div>
     </div>
   );
 }
