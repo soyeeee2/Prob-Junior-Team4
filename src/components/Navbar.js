2a7094668b55cd76f2c2dd0d51196356c7e1ec34
soyeeee2 <soyeeee2@gmail.com>
2022-07-22 16:17:00 +0900

    ddd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,12 +17,14 @@ const Navbar = () => {
             <li><a href="#!">여행지추천</a></li>
             <li><a href="#!">마이페이지</a></li>
           </ul>
-          <div className={styles.navSearch}>
-            <input className={styles.inputSearch} type="text" placeholder="지도로 검색하기"></input>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-            </svg>
-          </div>  
+
+            <div className={styles.navSearch}>
+              <div className={styles.inputSearch} type="text" placeholder="지도로 검색하기"><p>지도로 검색하기</p></div>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+              </svg>
+            </div>  
+
           <ul className={styles.login}>
             <li><a href="#!">LOGIN</a></li>
             <li><a href="#!">JOIN US</a></li>
