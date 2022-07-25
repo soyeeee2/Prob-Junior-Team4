c1ddc36c13de0f90d1e81cae33348ace9e26c43e
soyeeee2 <soyeeee2@gmail.com>
2022-07-25 16:55:01 +0900

    signup

--- src/routes/SignUpPage.js
+++ src/routes/SignUpPage.js
@@ -3,6 +3,7 @@ import {auth} from "../fbase"
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import Navbar from "../components/Navbar";
 import { useNavigate } from "react-router-dom";
+import styles from "../styles/SignUpPage.module.css"
 
 const SignUpPage = () => {
     const [email, setEmail] = useState('');
@@ -38,11 +39,16 @@ const SignUpPage = () => {
         }
       }
     return (
-        <div>
+      <div className={styles.conXS}>
+        <div className={styles.Logo}>
+          <img src={require('../assets/Logo.png')}/>  
+          <p>Travel</p>
+        </div>
         <form onSubmit={onSubmit}>
           <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
           <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
-          <p>비밀번호는 특수 문자, 영어, 숫자를 포함하여 8 ~ 15 글자로 설정해주세요</p>
+          <p>비밀번호는 8~15 글자로 작성해주시길 바랍니다.</p>
+          <p>&#40;특수 문자, 영어, 숫자포함&#41;</p>
           <input type="submit" value="Sign Up" />
         </form>
       </div>
